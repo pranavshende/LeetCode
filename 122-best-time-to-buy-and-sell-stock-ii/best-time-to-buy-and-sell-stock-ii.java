8eae1f646c52fdb8e9e7c21343bab657aa0d9185
Pranav shende <71146296+pranavshende@users.noreply.github.com>
2026-07-29 07:07:45 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (12.16%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -5,11 +5,11 @@ class Solution {
         int profit = 0;
 
         while (i + 1 < n) {
-         
+            // Walk down to the next valley
             while (i + 1 < n && prices[i] >= prices[i + 1]) i++;
             int valley = prices[i];
 
-     
+            // Walk up to the peak that follows
             while (i + 1 < n && prices[i] <= prices[i + 1]) i++;
             int peak = prices[i];
 
