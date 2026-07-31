2054768813ae7b0963fd2e6b28d93d34c6172f36
Pranav shende <71146296+pranavshende@users.noreply.github.com>
2026-07-31 22:36:02 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (93.72%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,19 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int i = 0;
         int profit = 0;
 
-        while (i + 1 < n) {
-            // Walk down to the next valley
-            while (i + 1 < n && prices[i] >= prices[i + 1]) i++;
-            int valley = prices[i];
-
-            // Walk up to the peak that follows
-            while (i + 1 < n && prices[i] <= prices[i + 1]) i++;
-            int peak = prices[i];
-
-            profit += peak - valley;
+        for (int i = 1; i < prices.length; i++) {
+            // Collect every upward price movement
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
         }
 
         return profit;
